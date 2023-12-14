bc7c9351ebd172f718fcfa84a913bd74aaaf021f
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-12-14 18:06:14 +0530

    define client side page and components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Home, Inbox, Mail, Settings } from '@mui/icons-material';
