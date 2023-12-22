4963f896f725642ebaa2747406fe6f4e39719e6e
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-12-22 11:13:09 +0530

    add search bar

--- src/components/addCotact.tsx
+++ src/components/addCotact.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Grid, Paper } from '@mui/material';
+import { Grid, TextField } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import UserCard from './userCard';
 import styles from './scroll.module.css';
@@ -13,6 +13,7 @@ export default function AddContact() {
     const { session } = useAuth();
 
     const [users, setUsers] = useState<UserInput[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
     useEffect(() => {
         GetAllProfiles(session)
@@ -26,17 +27,45 @@ export default function AddContact() {
             });
     }, [session]);
 
+    // Filter users based on search term
+    const filteredUsers = users.filter((user) =>
+        user.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
     return (
-        <Grid className={styles['custom-scroll-container']} container
-            sx={{ borderRadius: '6px 0 0 0', pt: '1rem', bgcolor: '#333', overflowY: 'auto' }}>
-            <Grid container justifyContent='center' spacing={2} sx={{ bgcolor: '#333' }}>
-                {users.map((user, index) => (
-                    <Grid item key={index} >
-                        <UserCard profileDetails={user} />
-                    </Grid>
-                ))}
+        <Grid container direction="column">
+            <Grid item sx={{ bgcolor: '#333', p: '1rem', display: 'flex', justifyContent: 'center' }}>
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    placeholder="Search by username, name, or email"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    InputProps={{
+                        style: {
+                            color: '#fff',
+                            borderRadius: '20px',
+                            border: '2px solid grey'
+                        }
+                    }}
+                    sx={{ pt: 3, pb: 3, maxWidth: '1050px', margin: 'auto' }}
+                />
+            </Grid>
+            <Grid item className={styles['custom-scroll-container']} sx={{ bgcolor: '#333', flex: 1, overflowY: 'auto' }}>
+                <Grid container justifyContent='center' spacing={2}>
+                    {filteredUsers.map((user, index) => (
+                        <Grid item key={index}>
+                            <UserCard profileDetails={user} />
+                        </Grid>
+                    ))}
+                </Grid>
             </Grid>
         </Grid>
-
     );
 }
