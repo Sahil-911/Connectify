46b7b833f6e1bfea4c0b4e4527e2079a4bd1ff10
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-12-28 18:52:53 +0530

    resolve dependency error

--- src/app/dashboard/manageConnections/page.tsx
+++ src/app/dashboard/manageConnections/page.tsx
@@ -1,101 +1,103 @@
 'use client';
 
-import React from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import { useTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
+import * as React from 'react';
 import Box from '@mui/material/Box';
-import styles from '@/components/scroll.module.css';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import Connections from './connections';
 import PendingConnections from './pendingConnections';
 import ConnectionRequests from './connectionRequests';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styles from '@/components/scroll.module.css';
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    dir?: string;
-    index: number;
-    value: number;
-}
+const theme = createTheme({
+    components: {
+        MuiTab: {
+            styleOverrides: {
+                root: {
+                    '&.Mui-selected': {
+                        color: '#007bff', // Text color of selected tab
+                    },
+                },
+            },
+        },
+        MuiList: {
+            styleOverrides: {
+                root: {
+                    borderBottom: '2px solid #007bff', // Indicator line color
+                },
+            },
+        },
+    },
+});
 
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
+export default function LabTabs() {
+    const [value, setValue] = React.useState('1');
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: number) {
-    return {
-        id: `full-width-tab-${index}`,
-        'aria-controls': `full-width-tabpanel-${index}`,
-    };
-}
-
-export default function FullWidthTabs() {
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
 
-    const handleChangeIndex = (index: number) => {
-        setValue(index);
-    };
-
     return (
-        <Box sx={{
-            bgcolor: '#333', width: '100%', height: '100%', borderRadius: '6px 0 0 0', backgroundImage: `url(/patternpad4.svg)`,
-            backgroundRepeat: 'repeat'
-        }}>
-            <AppBar position="static" sx={{ borderRadius: '6px 0 0 0' }}>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    indicatorColor="secondary"
-                    textColor="inherit"
-                    variant="fullWidth"
-                    aria-label="full width tabs example"
-                    sx={{ bgcolor: '#262626', borderRadius: '6px 0 0 0' }}
-                >
-                    <Tab label="Connenctions" {...a11yProps(0)} />
-                    <Tab label="Pending Connections" {...a11yProps(1)} />
-                    <Tab label="Connection Requests" {...a11yProps(2)} />
-                </Tabs>
-            </AppBar>
-            <SwipeableViews
+        <>
+            <Box
                 className={styles['custom-scroll-container']}
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={value}
-                onChangeIndex={handleChangeIndex}
-                style={{ color: '#fff', height: '100%', width: '100%', overflowY: 'auto' }}
-            >
-                <TabPanel value={value} index={0} dir={theme.direction}>
-                    <Connections />
-                </TabPanel>
-                <TabPanel value={value} index={1} dir={theme.direction}>
-                    <PendingConnections />
-                </TabPanel>
-                <TabPanel value={value} index={2} dir={theme.direction}>
-                    <ConnectionRequests />
-                </TabPanel>
-            </SwipeableViews>
-        </Box>
+                sx={{
+                    width: '100%',
+                    borderRadius: '6px 0 0 0',
+                    backgroundImage: `url(/patternpad4.svg)`,
+                    backgroundRepeat: 'repeat',
+                    overflowY: 'auto'
+                }}>
+                <TabContext value={value}>
+                    <ThemeProvider theme={theme}>
+                        <TabList
+                            centered
+                            onChange={handleChange}
+                            aria-label="lab API tabs example"
+                            indicatorColor="primary" // Set the indicator color to primary
+                            sx={{
+                                width: '100%',
+                                // display: 'flex',
+                                // justifyContent: 'space-between',
+                                bgcolor: '#262626',
+                                borderRadius: '6px 0 0 0',
+                                padding: 0
+                            }}
+                        >
+                            <Tab
+                                value="1"
+                                label="Connections"
+                                sx={{
+                                    color: value === '1' ? '#007bff' : '#fff',
+                                    width:'33%'
+                                }}
+                            />
+                            <Tab
+                                value="2"
+                                label="Pending Connections"
+                                sx={{
+                                    color: value === '2' ? '#007bff' : '#fff',
+                                    width:'33%'
+                                }}
+                            />
+                            <Tab
+                                value="3"
+                                label="Connection Requests"
+                                sx={{
+                                    color: value === '3' ? '#007bff' : '#fff',
+                                    width:'33%'
+                                }}
+                            />
+                        </TabList>
+                    </ThemeProvider>
+                    <TabPanel value="1"><Connections /></TabPanel>
+                    <TabPanel value="2"><PendingConnections /></TabPanel>
+                    <TabPanel value="3"><ConnectionRequests /></TabPanel>
+                </TabContext>
+            </Box>
+        </>
     );
 }
